4433a640ceef421a1c2b34ed6e0e07d2d0e4c271
Rashid ALi <49899864+rashid327@users.noreply.github.com>
2020-02-13 09:50:21 +0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <script src="https://www.paypalobjects.com/api/checkout.js"></script>
 </head>
-
+<!-- Content -->
 <body>
   <div id="paypal-button"></div>
   <script>
